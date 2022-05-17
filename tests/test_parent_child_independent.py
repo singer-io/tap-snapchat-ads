d00f9e4fc6f3e628fbe674530bf1a08d4fc56740
savan-chovatiya <savan.chovatiya@crestdatasys.com>
2022-05-17 18:59:37 +0530

    Resolved review comment

--- tests/test_parent_child_independent.py
+++ tests/test_parent_child_independent.py
@@ -6,17 +6,28 @@ class SnapchatParentChildIndependentTest(SnapchatBase):
     def name(self):
         return "tap_tester_snapchat_ads_parent_child_test"
 
-    def test_run(self):
+    def test_first_level_child_streams(self):
+        """
+            Test case to verify that tap is working fine if only first level child streams are selected
+        """
         # select first_level_child_streams only and run test
         first_level_child_streams = {"funding_sources", "billing_centers", "members", "roles", "ad_accounts", "product_catalogs"}
         self.run_test(first_level_child_streams)
 
+    def test_second_level_child_streams(self):
+        """
+            Test case to verify that tap is working fine if only second level child streams are selected
+        """
         # select second_level_child_streams only and run test
         second_level_child_streams = {"ad_account_stats_daily", "ad_account_stats_hourly", "audience_segments", "pixels", "media",
                                "creatives", "phone_numbers", "campaigns", "ad_squads", "ads", "product_sets"}
         # Don't have data for stats stream so excluding from test
         self.run_test(second_level_child_streams - self.stats_streams)
-        
+
+    def test_third_level_child_streams(self):
+        """
+            Test case to verify that tap is working fine if only third level child streams are selected
+        """
         # select third_level_child_streams only and run test
         third_level_child_streams = {"pixel_domain_stats", "campaign_stats_daily", "campaign_stats_hourly", "ad_squad_stats_daily",
                                      "ad_squad_stats_hourly", "ad_stats_daily", "ad_squad_stats_hourly"}
