bbd40a92fb954156b242183a89f3c5d9828ba742
Ben Allred <ben.allred@qlik.com>
2025-07-17 11:47:29 -0600

    SAC-27925: bump dependencies for compliance (#31)
    * Update setup.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(name='tap-snapchat-ads',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_snapchat_ads'],
       install_requires=[
-          'backoff==1.8.0',
-          'requests==2.31.0',
+          'backoff==1.10.0',
+          'requests==2.32.4',
           'pyhumps==1.3.1',
-          'singer-python==5.9.0'
+          'singer-python==5.13.2'
       ],
       entry_points='''
           [console_scripts]
