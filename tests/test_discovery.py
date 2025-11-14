3fae2351dc3f97e3cf13f6f8a7140238d03eaef9
Pagadala Narender <ext.pagadala.narender@globallogic.com>
2025-11-14 17:46:20 +0530

    changes for the parent steram intigration test 1

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -106,10 +106,6 @@ class SnapchatDiscoveryTest(SnapchatBase):
                         msg=f"{stream} has incorrect {self.PARENT_TAP_STREAM_ID}: "
                             f"expected '{expected_parent_stream_id}', got '{actual_parent_stream_id}'"
                     )
-                else:
-                    self.assertIsNone(actual_parent_stream_id,
-                        msg=f"{stream} should NOT have a parent but metadata returned '{actual_parent_stream_id}'")
-
                 # verify that all other fields have inclusion of available
                 # This assumes there are no unsupported fields for SaaS sources
                 self.assertTrue(
